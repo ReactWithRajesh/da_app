4d23b2e22d8002d24ca5ac9fe02775658e7e6dd7
Rajesh <rksmech79@gmail.com>
2023-12-27 00:46:55 +0530

    updated code

---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+// webpack.config.js
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+    context: __dirname,
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'main.js',
+        publicPath: '/'
+    },
+    devServer: {
+        historyApiFallback: true,
+        port:3222
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-react']
+                    }
+                },
+            },
+            {
+                test: /\.css?$/,
+                use: ['style-loader', 'css-loader']
+            },
+            {
+                test: /\.(png|j?g|svg|gif)?$/,
+                use: 'file-loader?name=./images/[name].[ext]'
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, 'public/index.html'),
+            filename: 'index.html'
+        })
+    ]
+};
