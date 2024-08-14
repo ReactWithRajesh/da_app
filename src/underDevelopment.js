b9ce4e69452cc164184619d8f3fb6f455f851f35
RAJESH KUMAR <49106379+ReactWithRajesh@users.noreply.github.com>
2024-08-14 10:45:26 +0000

    Updated

--- src/underDevelopment.js
+++ src/underDevelopment.js
@@ -8,8 +8,9 @@ export default function UnderDevelopment(props) {
             <Divider />
             <CardContent style={{ textAlign: 'center' }}>
                 <h1 style={{ color: '#00b0ff  ' }}><CelebrationIcon /> *** COMING SOON *** <CelebrationIcon /></h1>
+                <p>This aaplication will support Student data managment and list of items.</p>
                 <p>We are working on this page.</p>
-                <p>It will be availale soon.</p>
+                <p>It will be available soon.</p>
             </CardContent>
         </Card>
     )
